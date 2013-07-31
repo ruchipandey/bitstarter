fab6f483a41978027575589c09d50a5515027641
ruchipandey <ruchi@stanford.edu>
2013-07-31 14:30:13 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+ var content1 = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   response.send(content1);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
